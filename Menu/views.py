ae874ac6c0630c35e1722e3d200a4166cbba5ba6
GrigorKhachatryan <grigor777001@yandex.ru>
2019-09-08 02:09:05 +0300

    add comments and optimization

--- Menu/views.py
+++ Menu/views.py
@@ -1,47 +1,48 @@
 from django.shortcuts import render
 from .models import Menu
 from django.db.models import Q
-import json
 
 def menu(request):
-    ResultObject = []
+    # ResultList - вложенные списки формата [title,parent,depth]
+    ResultList = []
     # Изменение значение default на OR для БД
     Q.default = Q.OR
 
     # Получаем исходную URL
     url = request.path[1:].split('/')
-
+    # Q() для получения корневых пунктов меню
     ObjectsFilterParams = [Q(parent__iexact='null')]
-
+    # Добавление в ObjectsFilterParams всех parent = url
     for parents in url:
         ObjectsFilterParams.append(Q(parent__iexact=parents))
-
+    # Запрос к БД с использованием Q()
     for p in Menu.objects.filter(*ObjectsFilterParams):
-        ResultObject.append([p.title, p.parent, p.depth])
+        ResultList.append([p.title, p.parent, p.depth])
+    # Преобразование ResultList в список объектов формата [{title:'title, children:[{},{}]}]
+    ResultJSON = RenderJSON(ResultList)
 
+    return render(request, 'index.html',{'obj': ResultJSON})
 
-    my_list = ResultObject
-    null_list = []
+# Рекурсивная функция для создания вложенных объектов
+def creatdict(i, ResultJSON,PointList):
+    for j in PointList:
+        if j[1] == i[0]:
+            ResultJSON['children'].append({"title": j[0], 'children': []})
+            creatdict(j, ResultJSON['children'][0],PointList)
 
-    for i in my_list:
-        if i[2] == 0:
-            null_list.append(i)
-    my_list = [x for x in my_list if x not in null_list]
-    data = [{}] * len(null_list)
-
-    def creatdict(i, data):
-        for j in my_list:
-            if j[1] == i[0]:
-                data['children'].append({"title": j[0], 'children': []})
-
-                creatdict(j, data['children'][0])
-
+# Функция для создания списка объектов
+def RenderJSON(PointList):
+    # Создания списка с корневыми пунктами меню
+    RootPointList = [i for i in PointList if i[2] == 0]
+    # Удаление корневых пунктов из списка пунктов
+    PointList = [x for x in PointList if x not in RootPointList]
+    # Создание списка объектов
+    ResultJSON = [{}] * len(RootPointList)
+    # Заполнение списка объектов
     index = 0
-    for i in null_list:
-        data[index] = {"title": i[0], 'children': []}
-        creatdict(i, data[0])
+    for i in RootPointList:
+        ResultJSON[index] = {"title": i[0], 'children': []}
+        creatdict(i, ResultJSON[0],PointList)
         index += 1
-    print(data)
-    return render(request, 'index.html',{'obj': data})
+    return ResultJSON
 
-# Create your views here.
